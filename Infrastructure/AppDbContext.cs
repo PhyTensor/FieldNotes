709e6156fdc9f8f427d820ac56d94054f5e1779f
PhyTensor <phystroq@gmail.com>
2025-04-12 06:29:50 +0300

    set up ef core dbcontext

---
+++ Infrastructure/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Core;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System.Text.Json;
+
+namespace Infrastructure;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Note> Notes => Set<Note>();
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    // protected override void OnModelCreating(ModelBuilder modelBuilder)
+    // {
+    //     var stringListConverter = new ValueConverter<List<string>, string>(
+    //         v => JsonSerializer.Serialize(v, (JsonSerializerOptions?)null),
+    //         v => JsonSerializer.Deserialize<List<string>>(v) ?? new List<string>());
+    //
+    //     modelBuilder.Entity<Note>()
+    //         .Property(n => n.Tags)
+    //         .HasConversion(stringListConverter);
+    // }
+}
