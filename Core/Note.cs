8f4eec177a4411a7eaae443b317304cad032cde1
PhyTensor <phystroq@gmail.com>
2025-04-12 06:17:04 +0300

    create core entity

---
+++ Core/Note.cs
@@ -0,0 +1,11 @@
+﻿namespace Core;
+
+public class Note
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public List<string> Tags { get; set; } = new();
+    public string? Location { get; set; }
+}
