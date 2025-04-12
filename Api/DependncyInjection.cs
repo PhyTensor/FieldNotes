d865e1d4834228808b38d145efc142949ad28a4f
PhyTensor <phystroq@gmail.com>
2025-04-12 07:01:54 +0300

    remove secrets from appsettings.json file

--- Api/DependncyInjection.cs
+++ Api/DependncyInjection.cs
@@ -7,7 +7,7 @@ public static class DependncyInjection
 {
     public static WebApplicationBuilder AddInfrastructure(this WebApplicationBuilder builder)
     {
-        var connectionString = builder.Configuration.GetConnectionString("AppDbContext") ?? "Host=localhost;Database=fieldnotes;Username=postgres;Password=postgres";
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? "Host=localhost;Database=fieldnotes;Username=postgres;Password=postgres";
 
         builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
 
