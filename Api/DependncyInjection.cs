a7fabb80595cc9d999169efeeb6673cb7b6d8299
PhyTensor <phystroq@gmail.com>
2025-04-12 07:17:09 +0300

    change code to read from environment; set up environment variable in docker, .env file

--- Api/DependncyInjection.cs
+++ Api/DependncyInjection.cs
@@ -7,7 +7,15 @@ public static class DependncyInjection
 {
     public static WebApplicationBuilder AddInfrastructure(this WebApplicationBuilder builder)
     {
-        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? "Host=localhost;Database=fieldnotes;Username=postgres;Password=postgres";
+        // Trying to get the connection string from the environment variable
+        var envConnString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+
+        var fallback = builder.Configuration.GetConnectionString("DefaultConnection");
+
+        var connectionString = string.IsNullOrWhiteSpace(envConnString) ? fallback : envConnString;
+
+
+        // var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? "Host=localhost;Database=fieldnotes;Username=postgres;Password=postgres";
 
         builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
 
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 FieldNotes - A scientific observation logger
 
+#### Setting up the environment variable
+
+##### When running locally
+
+You can use the shell or in the `.env` file.
+
+```shell
+export DB_CONNECTION_STRING="Host=localhost;Database=fieldnotes;Username=postgres;Password=postgres"
+```
+
+Or if using the `.env` file, export it before running the application.
+
+```shell
+source .env
+dotnet run --project Api
+```
+
+##### When running in Docker
+
+Have the envrionment variable in the `docker-compose.yml` file.
+
