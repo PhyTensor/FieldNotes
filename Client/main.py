4bf1a6a877a645763032015dafbe8421332b5237
PhyTensor <phystroq@gmail.com>
2025-04-13 14:26:59 +0300

    trying something

--- Client/main.py
+++ Client/main.py
@@ -1,17 +1,17 @@
+import os
+
 import httpx
 from nicegui import ui
 
-API_URL = "http://localhost:5282"
+API_URL = os.environ.get("API_URL", "http://api:80")
 
-notes = []
+notes_list = ui.column()
 
 async def load_notes():
-    global notes
+    notes_list.clear()
     async with httpx.AsyncClient() as client:
-        resp = await client.get(f"{API_URL}/notes")
-        notes = resp.json()
-        notes_list.clear()
-        for note in notes:
+        response = await client.get(f"{API_URL}/notes")
+        for note in response.json():
             with notes_list:
                 ui.label(f"{note['title']}: {note['content']}")
 
@@ -22,14 +22,47 @@ async def create_note(title, content):
 
 ui.label("📓 Field Notes").classes("text-2xl font-bold")
 ui.separator()
-
 title = ui.input("Title").props("outlined")
 content = ui.input("Content").props("outlined type=textarea")
-
 ui.button("Add Note", on_click=lambda: create_note(title.value, content.value))
-
-notes_list = ui.column()
 ui.button("Reload Notes", on_click=load_notes)
 
-ui.run(title="Field Notes UI", reload=False)
+ui.run(title="Field Notes UI", reload=False, port=8080, host="0.0.0.0")
+
+
+
+# import httpx
+# from nicegui import ui
+#
+# API_URL = "http://localhost:5282"
+#
+# notes = []
+#
+# async def load_notes():
+#     global notes
+#     async with httpx.AsyncClient() as client:
+#         resp = await client.get(f"{API_URL}/notes")
+#         notes = resp.json()
+#         notes_list.clear()
+#         for note in notes:
+#             with notes_list:
+#                 ui.label(f"{note['title']}: {note['content']}")
+#
+# async def create_note(title, content):
+#     async with httpx.AsyncClient() as client:
+#         await client.post(f"{API_URL}/notes", json={"title": title, "content": content})
+#     await load_notes()
+#
+# ui.label("📓 Field Notes").classes("text-2xl font-bold")
+# ui.separator()
+#
+# title = ui.input("Title").props("outlined")
+# content = ui.input("Content").props("outlined type=textarea")
+#
+# ui.button("Add Note", on_click=lambda: create_note(title.value, content.value))
+#
+# notes_list = ui.column()
+# ui.button("Reload Notes", on_click=load_notes)
+#
+# ui.run(title="Field Notes UI", reload=False)
 
---
+++ test/NoteServiceTests.cs
@@ -0,0 +1,10 @@
+namespace test;
+
+public class NoteServiceTests
+{
+    [Fact]
+    public async Task CreateAsync_ShouldCreateANote()
+    {
+
+    }
+}
